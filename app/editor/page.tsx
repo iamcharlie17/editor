6fbe1147563d16b9a2965d1c776aeed10bcbecca
iamcharlie17 <riyadhosen40@gmail.com>
2025-10-08 22:35:12 +0600

    create: editor-component folder

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,51 +1,12 @@
-"use client"
+'use client';
 
-import { useState } from "react"
-import { SerializedEditorState } from "lexical"
-
-import { Editor } from "@/app/editor/components/blocks/editor-x/editor"
-
-export const initialValue = {
-  root: {
-    children: [
-      {
-        children: [
-          {
-            detail: 0,
-            format: 0,
-            mode: "normal",
-            style: "",
-            text: "",
-            type: "text",
-            version: 1,
-          },
-        ],
-        direction: "ltr",
-        format: "",
-        indent: 0,
-        type: "paragraph",
-        version: 1,
-      },
-    ],
-    direction: "ltr",
-    format: "",
-    indent: 0,
-    type: "root",
-    version: 1,
-  },
-} as unknown as SerializedEditorState
-
-export default function EditorPage() {
-  const [editorState, setEditorState] = useState<SerializedEditorState>(initialValue)
+import EditorForm from "@/editor-component/EditorForm";
 
 
+const Page = () => {
   return (
-    <>
-      <Editor
-        editorSerializedState={editorState}
-        onSerializedChange={(value) => setEditorState(value)}
-      />
-       <button onClick={() => console.log(editorState)} className="bg-blue-500 text-white px-4 py-2 rounded">Save</button>
-    </>
-  )
-}
+    <EditorForm/>
+  );
+};
+
+export default Page;
